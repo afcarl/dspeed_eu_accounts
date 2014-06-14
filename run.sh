6eacbaba7942622e96e50bbf85495eee90c8b3a4
Frank Lemanschik <frank@dspeed.eu>
2014-06-14 12:38:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run ubuntu:14.04 /bin/bash -c echo bla"]
-docker run ubuntu:14.04 /bin/bash -c echo bla"]
+docker run -d ubuntu:14.04 /bin/bash -c echo bla"]
+docker run -d ubuntu:14.04 /bin/bash -c echo bla"]
 docker images
-docker p -a
+docker ps -a
